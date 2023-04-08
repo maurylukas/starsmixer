5af26a89d6b3847af23047b2ad74ba96946042ee
Maury Lukas Oliveira Freitas <97759783+maurylukas@users.noreply.github.com>
2023-04-08 07:40:32 -0300

    Solved issue
    Fixing deploying problem

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ if __name__ == '__main__':
     app.run()
 
 # before deploying use the following commands on terminal:
-# 1) pip3 install pipreqs
-# 2) pip3 install pip-tools
-# 3) pipreqs --savepath=requirements.in
-# 4) pip-compile
+# 1) pip3 install gunicorn
+# 2) pip3 install pipreqs
+# 3) pip3 install pip-tools
+# 4) pipreqs --savepath=requirements.in
+# 5) pip-compile
 # they will create two requirements files needed for deployment
+# in the end of the requirements.txt file add this:
+# gunicorn==20.1.0
+# create a new file called Procfile and write within it:
+# web: gunicorn main:app
