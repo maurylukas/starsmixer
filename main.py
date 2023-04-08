10039539fb74ac853b673aa207f15a64c23ab680
Maury Lukas Oliveira Freitas <97759783+maurylukas@users.noreply.github.com>
2023-04-08 07:13:09 -0300

    Added requirements
    Preparing for first deploy

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from sm import app
 # running website
 if __name__ == '__main__':
     app.run()
+
+# before deploying use the following commands on terminal:
+# 1) pip3 install pipreqs
+# 2) pip3 install pip-tools
+# 3) pipreqs --savepath=requirements.in
+# 4) pip-compile
+# they will create two requirements files needed for deployment
